c27c4aee79cf770b54f24665dd36c67e2efa3457
MathwlL <liramatheus720@gmail.com>
2026-03-26 23:16:04 -0300

    Adicionar arquivos de projeto.

---
+++ CarMark/wwwroot/js/Home.js
@@ -0,0 +1,26 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const buttons = document.querySelectorAll(".filter-buttons button");
+    const cards = document.querySelectorAll(".card");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+
+            buttons.forEach(btn => btn.classList.remove("active"));
+
+            button.classList.add("active");
+
+            const filter = button.getAttribute("data-filter");
+
+            cards.forEach(card => {
+                if (filter === "all" || card.classList.contains(filter)) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
+                }
+            });
+
+        });
+    });
+
+});
