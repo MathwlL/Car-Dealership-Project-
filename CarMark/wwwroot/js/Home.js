1904d02d084b83068edb2b14c0355f262632b153
MathwlL <liramatheus720@gmail.com>
2026-03-28 01:40:37 -0300

    Home completed, privacy work start

--- CarMark/wwwroot/js/Home.js
+++ CarMark/wwwroot/js/Home.js
@@ -24,3 +24,10 @@
     });
 
 });
+const items = document.querySelectorAll('.faq-item');
+
+items.forEach(item => {
+    item.querySelector('.faq-question').addEventListener('click', () => {
+        item.classList.toggle('active');
+    });
+});
